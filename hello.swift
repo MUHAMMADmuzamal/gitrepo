2231eda8c2a5c9facb7f4dfee03fe2f2feabd1d6
Muhammad Muzamal <muhammadmuzamal42@gmail.com>
2023-09-07 17:54:26 +0500

    track remote branch from local

--- hello.swift
+++ hello.swift
@@ -3,4 +3,4 @@ dev branch created.
 local change in dev
     new line to dev
 remote change file
-another line in dev from remote
+another line in dev from remote now pull remote branch to local and now tracking in local
